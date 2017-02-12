f0cfeccd7f9338349ff81aee59e1bc1f9c0b4ec3
jeaye <contact@jeaye.com>
2017-02-12 11:06:33 -0800

    Add word list reading

--- src/wordy_word/core.clj
+++ src/wordy_word/core.clj
@@ -1,7 +1,19 @@
 (ns wordy-word.core
   (:gen-class))
 
+(defn read-lines [file]
+  (try
+    (-> (clojure.java.io/resource file)
+        slurp
+        clojure.string/split-lines)
+    (catch Throwable _
+      [])))
+
+(def unapproved-nouns (atom (read-lines "unapproved-nouns")))
+(def unapproved-adjectives (atom (read-lines "unapproved-adjectives")))
+(def approved-nouns (atom (read-lines "approved-nouns")))
+(def approved-adjectives (atom (read-lines "approved-adjectives")))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let []))
