d7f1c3c67029028418aad437fdb9fc078720f0f2
jeaye <contact@jeaye.com>
2017-02-12 11:42:35 -0800

    Add initial random name generation

--- src/wordy_word/core.clj
+++ src/wordy_word/core.clj
@@ -14,6 +14,27 @@
 (def approved-nouns (atom (read-lines "approved-nouns")))
 (def approved-adjectives (atom (read-lines "approved-adjectives")))
 
+; TODO: Refactor to rand-word
+(defn rand-noun [generated]
+  (if (empty? @approved-nouns)
+    generated
+    (conj generated (rand-nth @approved-nouns))))
+(defn rand-adjective [generated]
+  (if (empty? @approved-adjectives)
+    generated
+    (conj generated (rand-nth @approved-adjectives))))
+
+; TODO: Alliterations
+(def generators [[rand-noun]
+                 [rand-noun rand-noun]
+                 [rand-adjective rand-noun]
+                 [rand-adjective rand-adjective rand-noun]])
+
+(defn generate []
+  (let [generator (rand-nth generators)
+        generated (reduce #(%2 %1) [] generator)]
+    generated))
+
 (defn -main
   [& args]
   (let []))
