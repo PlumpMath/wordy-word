f31156dc85827412409d117c2ea2a8f01ef7ef92
jeaye <contact@jeaye.com>
2017-02-12 14:22:16 -0800

    Add alliterative generation

--- src/wordy_word/core.clj
+++ src/wordy_word/core.clj
@@ -11,22 +11,40 @@
 
 (def unapproved-nouns (atom (read-lines "unapproved-nouns")))
 (def unapproved-adjectives (atom (read-lines "unapproved-adjectives")))
-(def approved-nouns (atom (read-lines "approved-nouns")))
-(def approved-adjectives (atom (read-lines "approved-adjectives")))
+(def approved-nouns (atom (read-lines "unapproved-nouns")))
+(def approved-adjectives (atom (read-lines "unapproved-adjectives")))
 
 (defn rand-word [word-list generated]
   (if (empty? @word-list)
     generated
     (conj generated (rand-nth @word-list))))
 
+(defn rand-alliterative-word [word-list generated]
+  (cond
+    (empty? @word-list) generated
+    (empty? generated) (rand-word word-list generated) ; No prior word to follow
+    :else
+    (let [word (rand-nth generated)
+          letter (first word)
+          possible-alliteration (filter #(= letter (first %)) @word-list)]
+      (if (empty? possible-alliteration)
+        generated
+        (conj generated (rand-nth possible-alliteration))))))
+
 (def rand-noun (partial rand-word approved-nouns))
 (def rand-adjective (partial rand-word approved-adjectives))
+(def rand-alliterative-noun (partial rand-alliterative-word approved-nouns))
+(def rand-alliterative-adjective (partial rand-alliterative-word approved-adjectives))
 
-; TODO: Alliterations
 (def generators [[rand-noun]
                  [rand-noun rand-noun]
+                 [rand-noun rand-alliterative-noun]
                  [rand-adjective rand-noun]
-                 [rand-adjective rand-adjective rand-noun]])
+                 [rand-adjective rand-alliterative-noun]
+                 [rand-adjective rand-adjective rand-noun]
+                 [rand-adjective rand-alliterative-adjective rand-noun]
+                 [rand-adjective rand-adjective rand-alliterative-noun]
+                 [rand-adjective rand-alliterative-adjective rand-alliterative-noun]])
 
 (defn generate []
   (let [generator (rand-nth generators)
