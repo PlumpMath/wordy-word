b4cdf1857ad25ecf659e8c82e69ed604385d3532
jeaye <contact@jeaye.com>
2017-02-12 21:05:10 -0800

    Update project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject wordy-word "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A silly name generator"
+  :url "https://github.com/jeaye/wordy-word"
   :license {:name "jank license"
             :url "https://upload.jeaye.com/jank-license"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
