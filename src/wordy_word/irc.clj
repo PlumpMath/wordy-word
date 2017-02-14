5b4ebd25642b7da49503883ea00ff14305933140
jeaye <contact@jeaye.com>
2017-02-13 21:51:18 -0800

    Lay out vote/stop/gen commands

--- src/wordy_word/irc.clj
+++ src/wordy_word/irc.clj
@@ -1,5 +1,6 @@
 (ns wordy-word.irc
-  (:require [irclj.core :as irc]))
+  (:require [irclj.core :as irc]
+            [clojure.pprint :refer [pprint]]))
 
 (def nick "wordy-word")
 (def channel "#wordy-word")
@@ -9,16 +10,36 @@
 (defn eat-log [& args]
   (comment pprint args))
 
+(defn message! [msg]
+  (irc/message @connection channel msg))
+
+(defn vote! [args]
+  (message! "ready to vote"))
+
+(defn stop! [args]
+  (message! "stopping"))
+
+(defn gen! [args]
+  (message! (pr-str ["sugar" "nanny"])))
+
+(def commands {:vote vote!
+               :stop stop!
+               :gen gen!})
+
+(defn on-msg [irc msg]
+  (when-let [command (re-matches #"\.(vote|stop|gen) ?(.*)" (:text msg))]
+    ((get commands (keyword (second command)))
+     (drop 2 command))))
+
 (defn disconnect! []
   (when @connection
     (swap! connection irc/kill)))
 
 (defn connect! []
   (disconnect!)
-  (reset! connection (irc/connect "irc.freenode.net"
-                                  6667 nick
-                                  :callbacks {:raw-log eat-log}))
+  (reset! connection
+          (irc/connect "irc.freenode.net"
+                       6667 nick
+                       :callbacks {:raw-log eat-log
+                                   :privmsg on-msg}))
   (irc/join @connection channel))
-
-(defn message! [msg]
-  (irc/message @connection channel msg))
