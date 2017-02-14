6c6ea37041d5bd9372531ed132d1ac356599b6b7
jeaye <contact@jeaye.com>
2017-02-13 22:51:26 -0800

    Add ballot creation

--- src/wordy_word/irc.clj
+++ src/wordy_word/irc.clj
@@ -1,7 +1,9 @@
 (ns wordy-word.irc
   (:require [wordy-word
-             [generate :refer [generate]]]
+             [generate :refer [generate]]
+             [word-list :as words]]
             [irclj.core :as irc]
+            [clojure.string :refer [join]]
             [clojure.pprint :refer [pprint]]))
 
 (def nick "wordy-word")
@@ -15,23 +17,41 @@
 (defn message! [msg]
   (irc/message @connection channel msg))
 
+; TODO: vote namespace
 (def ballot (atom []))
 
-(defn vote! [args]
-  (message! "ready to vote"))
+(defn take-rand [n coll]
+  (take n (shuffle coll)))
 
-(defn stop! [args]
-  (message! "stopping"))
+(defn make-ballot [kind size]
+  (take-rand size (if (= :noun kind)
+                    @words/unapproved-nouns
+                    @words/unapproved-adjectives)))
+
+(defn strip-yes-no [yes-no-str]
+  (apply str (filter #(or (= % \y) (= % \n)) yes-no-str)))
+
+(defn vote! [args]
+  (pprint args)
+  (let [start (re-matches #"(noun|verb) (\d+)" (first args))
+        complete (re-matches #"(y|n)+" (strip-yes-no (first args)))]
+    (cond
+      start (let [kind (keyword (second start))
+                  size (Integer/parseInt (nth start 2))]
+              (reset! ballot (make-ballot kind size))
+              (message! (str "ballot: " (join ", " @ballot))))
+      complete (let []
+                 )
+      :else (message! "invalid vote command"))))
 
 (defn gen! [args]
-  (message! (clojure.string/join " " (generate))))
+  (message! (join " " (generate))))
 
 (def commands {:vote vote!
-               :stop stop!
                :gen gen!})
 
 (defn on-msg [irc msg]
-  (when-let [command (re-matches #"\.(vote|stop|gen) ?(.*)" (:text msg))]
+  (when-let [command (re-matches #"\.(vote|gen) ?(.*)" (:text msg))]
     ((get commands (keyword (second command)))
      (drop 2 command))))
 
